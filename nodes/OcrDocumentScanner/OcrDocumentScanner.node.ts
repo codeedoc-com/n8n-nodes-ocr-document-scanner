c504f868e03a14cbe879aaab0a867ecb8c2e9418
Om Codeedoc <om.viradiya@codeedoc.com>
2026-02-28 16:17:13 +0530

    first commit

---
+++ nodes/OcrDocumentScanner/OcrDocumentScanner.node.ts
@@ -0,0 +1,50 @@
+import { NodeConnectionTypes, type INodeType, type INodeTypeDescription } from 'n8n-workflow';
+import { userDescription } from './resources/user';
+import { companyDescription } from './resources/company';
+
+export class OcrDocumentScanner implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Ocr Document Scanner',
+		name: 'ocrDocumentScanner',
+		icon: { light: 'file:ocrDocumentScanner.svg', dark: 'file:ocrDocumentScanner.dark.svg' },
+		group: ['transform'],
+		version: 1,
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		description: 'Interact with the Ocr Document Scanner API',
+		defaults: {
+			name: 'Ocr Document Scanner',
+		},
+		usableAsTool: true,
+		inputs: [NodeConnectionTypes.Main],
+		outputs: [NodeConnectionTypes.Main],
+		credentials: [],
+		requestDefaults: {
+			baseURL: 'https://janet-submissions-games-wives.trycloudflare.com',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		},
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'User',
+						value: 'user',
+					},
+					{
+						name: 'Company',
+						value: 'company',
+					},
+				],
+				default: 'user',
+			},
+			...userDescription,
+			...companyDescription,
+		],
+	};
+}
